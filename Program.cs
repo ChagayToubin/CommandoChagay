3dc1dcf3f0f2f5a3e321b65de334b337f2994612
chagay <chagaytoubin@gmail.com>
2025-05-27 11:35:42 +0300

    Finish Airecommando

--- Program.cs
+++ Program.cs
@@ -6,17 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
             
-            Commandoes Solider = new Commandoes("chagay", "123456");
-            Console.WriteLine(Solider.codename);
-            Solider.codename = "21";
-
-            Console.WriteLine(Solider.codename);
             
             //check();
         }
         public static void check()
         {
-            Commandoes Solider = new Commandoes("chagay", "123456");
+            Commandoes Solider = new Commandoes("chagay","123456");
             Console.WriteLine(Solider.codename + "  " + Solider.SayName("as"));
 
             Weapon weapon = new Weapon("Handgun", "Rafael", 100);
